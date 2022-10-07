1e9299f9cea671a71f95bce140a0c6bc9acf39aa
Yaqub naqeb <yaqub.009448401@gmail.com>
2022-10-07 16:45:16 +0300

    adding users lmits

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -7,12 +7,13 @@ import {
     BrowserRouter as Router,
     Routes,
     Route} from "react-router-dom";
+    import { Navigate } from 'react-router-dom'
 import Navbar from '../components/navigation/Navbar';
 import CardInfo from '../components/cards/CardInfo';
 import Login from './form/Login';
   
 const Main = () => {
-  const {authIsReady}=useAuthContext();
+  const {authIsReady,user}=useAuthContext();
 
   return (
     <div>
@@ -25,11 +26,17 @@ const Main = () => {
 <Navbar/>
 
  <Routes>
-  <Route path='/' element={<Home/>}/>
+ <Route path='/' element={<Home/>}/> 
+ 
+
+
   <Route path='/library' element={<Library/>}/>
   <Route path='/cardinfo/:id'  element={<CardInfo/>} />
-  <Route path='/signup' element={<SignUp/>}/>
-  <Route path='/login' element={<Login/>}></Route>
+  <Route path='/signup' element={user?<Navigate to={'/'}/>: <SignUp/>} >
+ 
+  </Route>
+  <Route path='/login' element={user?<Navigate to={'/'}/>: <Login/>} >
+  </Route>
   </Routes> 
 
  </Router>
