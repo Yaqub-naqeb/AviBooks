4ceeccec91aff7cd4917a670e04b0bdc62bfb4e1
Yaqub naqeb <yaqub.009448401@gmail.com>
2022-10-24 22:37:19 +0300

    adding book

--- src/components/firebase/config.js
+++ src/components/firebase/config.js
@@ -1,7 +1,7 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
-
+// kon
 const firebaseConfig = {
   apiKey: "AIzaSyAJxX8rNDty5vVYcDEW50hRpew-U4aWAyE",
   authDomain: "book-e8586.firebaseapp.com",
@@ -10,6 +10,15 @@ const firebaseConfig = {
   messagingSenderId: "881351900538",
   appId: "1:881351900538:web:97010d8c01159c9397eac3"
 };
+// new
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBUeLdbEQlF7omj3F-_9w5VCywmOjk-SFE",
+//   authDomain: "newbook-3d9d0.firebaseapp.com",
+//   projectId: "newbook-3d9d0",
+//   storageBucket: "newbook-3d9d0.appspot.com",
+//   messagingSenderId: "277918705788",
+//   appId: "1:277918705788:web:20290797fb5923897ec1f9"
+// };
 
 //init firebase
 firebase.initializeApp(firebaseConfig)
@@ -17,5 +26,9 @@ firebase.initializeApp(firebaseConfig)
 //init serbices
 const projectFirestore=firebase.firestore();
 const projectAuth=firebase.auth();
-export {projectFirestore,projectAuth}
+
+//timestamp
+  const timestamp=firebase.firestore.timestamp
+
+export {projectFirestore,projectAuth,timestamp}
 
--- src/pages/Adding.js
+++ src/pages/Adding.js
@@ -1,9 +1,14 @@
 import React from "react";
 import AddingBook from "../components/bookAdding/AddingBook";
+import { useAuthContext } from "../components/store/useAuthContext";
+
+
 const Adding = () => {
+  const {user}=useAuthContext ();
+
   return (
     <div className="bg-[#2c3562] pt-[5rem] ">
-      <AddingBook />
+      <AddingBook uid={user.uid}/>
     </div>
   );
 };
