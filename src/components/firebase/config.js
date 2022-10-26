976630a1cb692e963f3edcd04f11ab589ea20091
Yaqub naqeb <yaqub.009448401@gmail.com>
2022-10-26 16:52:53 +0300

    solving some problem

--- src/components/firebase/config.js
+++ src/components/firebase/config.js
@@ -10,15 +10,7 @@ const firebaseConfig = {
   messagingSenderId: "881351900538",
   appId: "1:881351900538:web:97010d8c01159c9397eac3"
 };
-// new
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBUeLdbEQlF7omj3F-_9w5VCywmOjk-SFE",
-//   authDomain: "newbook-3d9d0.firebaseapp.com",
-//   projectId: "newbook-3d9d0",
-//   storageBucket: "newbook-3d9d0.appspot.com",
-//   messagingSenderId: "277918705788",
-//   appId: "1:277918705788:web:20290797fb5923897ec1f9"
-// };
+
 
 //init firebase
 firebase.initializeApp(firebaseConfig)
