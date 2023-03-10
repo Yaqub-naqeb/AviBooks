bf2d2983479a0f50b86a20274384493a9a51bfdc
Yaqub naqeb <yaqub.009448401@gmail.com>
2023-03-10 21:23:33 +0300

    add

--- src/components/bookAdding/AddingBook.js
+++ src/components/bookAdding/AddingBook.js
@@ -68,6 +68,26 @@ onChange={(e)=>setTitle(e.target.value)}
 value={title}
 
 type="Name" placeholder='Book pages' required/>
+{/* pages */}
+<input
+
+className=' pl-3  py-2 cursor-pointer hover:border-[#2C3562]  border rounded-lg shadow-lg'
+
+
+onChange={(e)=>setTitle(e.target.value)}
+value={title}
+
+type="Name" placeholder='Book pages' required/>
+{/* pages */}
+<input
+
+className=' pl-3  py-2 cursor-pointer hover:border-[#2C3562]  border rounded-lg shadow-lg'
+
+
+onChange={(e)=>setTitle(e.target.value)}
+value={title}
+
+type="Name" placeholder='Book pages' required/>
 {/* author */}
 <input
 className=' hover:border-[#2C3562] border pl-3 py-2 my-3 cursor-pointer   rounded-lg shadow-lg'
@@ -75,13 +95,7 @@ className=' hover:border-[#2C3562] border pl-3 py-2 my-3 cursor-pointer   rounde
 onChange={(e)=>setAuthor(e.target.value)}
 value={author}
 type="" placeholder='Author' />
-{/* author */}
-<input
-className=' hover:border-[#2C3562] border pl-3 py-2 my-3 cursor-pointer   rounded-lg shadow-lg'
 
-onChange={(e)=>setAuthor(e.target.value)}
-value={author}
-type="" placeholder='Author' />
 <input
 
 className='pl-3 hover:border-[#2C3562] rounded-sm py-2 shadow-lg'
@@ -100,9 +114,6 @@ type="number" placeholder='Price' />
 <div className=' flex gap-5 self-center'>
 <button className='px-[1rem] cursor-pointer mt-10 py-[1rem] text-[#ffff] bg-slate-800 w-[9rem]  text-center flex items-center justify-center align-middle self-center text-xl rounded-md hover:shadow-md hover:bg-slate-700 transition-all duration-300 ease-in-out shadow-lg ' >
     Add book
-</button>
-<button className='px-[1rem] cursor-pointer mt-10 py-[1rem] text-[#ffff] bg-slate-800 w-[9rem]  text-center flex items-center justify-center align-middle self-center text-xl rounded-md hover:shadow-md hover:bg-slate-700 transition-all duration-300 ease-in-out shadow-lg ' >
-    Delete
 </button>
 
 
